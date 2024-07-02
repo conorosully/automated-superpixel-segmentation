e1935be8caadfc56fe4b5e96207378d0a8c4f6c0
conorosully <conorosullivan@conor.local>
2024-07-02 15:23:33 +0100

    updated evalution code

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,7 +1,9 @@
 from scipy.ndimage import distance_transform_edt
 import matplotlib.pyplot as plt
 import numpy as np
-#from train_landsat_unet import *  # load dataset and model classes
+from train import *  # load dataset and model classes
+import argparse
+
 
 
 import utils
@@ -43,11 +45,19 @@ def permutate_bands(img, bands):
 
     return img
 
+class Args:
+    def __init__(self, target_pos, incl_bands, satellite):
+        self.target_pos = target_pos
+        self.incl_bands = incl_bands
+        self.satellite = satellite
 
-def get_preds(model, test_paths, batch_size=10, target_pos=-2, bands=None):
+
+def get_preds(model, test_paths, batch_size=10, target_pos=-1, incl_bands=[0, 1, 2, 3, 4, 5, 6], satellite="landsat",perm_bands=None,device='mps'):
     """Get model predictions for a given dataloader"""
-
-    test_data = TrainDataset(test_paths, target_pos)
+    
+    
+    args = Args(target_pos, incl_bands, satellite)
+    test_data = TrainDataset(test_paths, args)
     test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=False)
     sm = nn.Softmax(dim=1)
 
@@ -56,9 +66,9 @@ def get_preds(model, test_paths, batch_size=10, target_pos=-2, bands=None):
 
     for images, target in iter(test_loader):
         # permutate bands
-        if bands != None:
+        if perm_bands != None:
             for i in range(len(images)):
-                images[i] = permutate_bands(images[i], bands)
+                images[i] = permutate_bands(images[i], perm_bands)
 
         images = images.to(device)
 
